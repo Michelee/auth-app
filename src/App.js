2023292532bd24287e50544cf40a634d3820a025
Michele Ancheta <michelee.ancheta@gmail.com>
2019-03-17 16:08:39 -0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB2_u6KzppuFvgyAvvhq66YI66CpNW6xe8',
+      authDomain: 'react-native-auth-6f54c.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-6f54c.firebaseio.com',
+      projectId: 'react-native-auth-6f54c',
+      storageBucket: 'react-native-auth-6f54c.appspot.com',
+      messagingSenderId: '115664664453'
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header headerText="Header Authentication" />
+        <Header headerText='Header Authentication' />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
       </View>
     );
